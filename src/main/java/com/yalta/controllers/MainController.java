535757e378cd4ac1c9b787e116432e035215e920
sidailen.me <sidailen.me@gmail.com>
2020-12-03 17:01:01 +0300

    done 3.0

--- src/main/java/com/yalta/controllers/MainController.java
+++ src/main/java/com/yalta/controllers/MainController.java
@@ -4,12 +4,15 @@ import com.yalta.service.interfaces.NumberService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -19,6 +22,12 @@ public class MainController {
 
     @PostMapping("/")
     public ResponseEntity<Map<Integer, Integer>> takeList(@RequestBody List<Integer> numbers) {
+        return new ResponseEntity<>(numberService.test(numbers), HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Map<Integer, Integer>> takeList() {
+        List<Integer> numbers = new Random().ints(0, 500).limit(200).boxed().collect(Collectors.toList());
         return new ResponseEntity<>(numberService.test(numbers), HttpStatus.OK);
     }
 }
